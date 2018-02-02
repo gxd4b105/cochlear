c10de50cc610ca1d4871beadb57b6c05dd562ca7
estaguilar <estaguilar@deloitte.com.au>
2018-02-02 18:43:09 +1100

    Ref FED Redux - "Related articles block"

--- src/components/HeaderLandingBanner/HeaderLandingBanner.jsx
+++ src/components/HeaderLandingBanner/HeaderLandingBanner.jsx
@@ -12,7 +12,7 @@ class HeaderLandingBanner extends React.Component {
                         <span className="content-grouper">
                             <h1>{this.props.title}</h1>
                             <p>{this.props.description}</p>
-                            <img class="arrow" src="/dist/assets/images/icons/Icon_Purple_Arrow.svg"></img>
+                            <img className="arrow" src="/dist/assets/images/icons/Icon_Purple_Arrow.svg"></img>
                         </span>
                     </div>
 
