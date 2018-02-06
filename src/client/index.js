4bfa55ed6e299e7b0e98b51a5c4deeff090e264a
David Falconer <dfalconer@deloitte.com.au>
2018-02-06 22:13:36 +1100

    Add require context to allow mass require of files

--- src/client/index.js
+++ src/client/index.js
@@ -5,73 +5,11 @@ import { Provider } from 'react-redux';
 import store from '../shared/app/redux/store';
 import App from '../shared/app/app.jsx';
 
-/* Logos */
-require('../images/logo_desktop.png');
-require('../images/logo_mobile.png');
-require('../images/logo_white.png');
+function importAll (r) {
+    r.keys().forEach(r);
+  }
 
-/* Icons */
-require('../images/Icon_Facebook@2x.png');
-require('../images/Icon_Google_@2x.png');
-require('../images/Icon_Twitter@2x.png');
-
-require('../images/Social_White_Facebook@2x.png');
-require('../images/Social_White_Google@2x.png');
-require('../images/Social_White_Twitter@2x.png');
-require('../images/Social_White_Youtube@2x.png');
-
-require('../images/Icon_White_Chevron@2x.png');
-require('../images/Icon_Yellow_Chevron@2x.png');
-require('../images/Icon_Purple_Chevron@2x.png');
-require('../images/Icon_Purple_Search@2x.png');
-require('../images/Icon_Purple_Arrow@2x.png');
-
-require('../images/Icon_Facebook.svg');
-require('../images/Icon_Google_.svg');
-require('../images/Icon_Twitter.svg');
-
-require('../images/Social_White_Facebook.svg');
-require('../images/Social_White_Google.svg');
-require('../images/Social_White_Twitter.svg');
-
-require('../images/Icon_White_Chevron.svg');
-require('../images/Icon_Yellow_Chevron.svg');
-require('../images/Icon_Purple_Chevron.svg');
-require('../images/Icon_Grey_Chevron.svg');
-
-require('../images/Icon_White_Search.svg');
-require('../images/Icon_Yellow_Search.svg');
-require('../images/Icon_Purple_Search.svg');
-require('../images/Icon_Grey_Search.svg');
-
-require('../images/Icon_White_Arrow.svg');
-require('../images/Icon_Yellow_Arrow.svg');
-require('../images/Icon_Purple_Arrow.svg');
-require('../images/Icon_Grey_Arrow.svg');
-
-require('../images/Icon_White_Download.svg');
-require('../images/Icon_Purple_Download.svg');
-require('../images/Icon_Grey_Download.svg');
-
-require('../images/Icon_White_Close.svg');
-require('../images/Icon_Yellow_Close.svg');
-require('../images/Icon_Purple_Close.svg');
-require('../images/Icon_Grey_Close.svg');
-
-require('../images/Icon_White_PDF.svg');
-require('../images/Icon_Yellow_PDF.svg');
-require('../images/Icon_Purple_PDF.svg');
-require('../images/Icon_Grey_PDF.svg');
-
-require('../images/Icon_White_Phone.svg');
-require('../images/Icon_Yellow_Phone.svg');
-require('../images/Icon_Purple_Phone.svg');
-
-require('../images/Icon_Yellow_Mail.svg');
-require('../images/Icon_Yellow_Location.svg');
-require('../images/Icon_Yellow_Time.svg');
-require('../images/Icon_Youtube.svg');
-require('../images/CLTD_INSPIRING_STORIES_USA_CHAD_Still004.jpg');
+importAll(require.context('../images/', true, /\.(png|svg|jpg)$/));
 
 /* Styles */
 require('../styles/global.scss');
